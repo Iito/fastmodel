dba5a833a19c782982cc76203d3aeb165fefecf0
Mansa Camara <mansa.camara.jp@gmail.com>
2025-07-25 23:17:53 +0900

    fix: Raising error when a Pydantic model is provided with no annotation and no IO given in args

--- src/fastmodel/utils/async_model.py
+++ src/fastmodel/utils/async_model.py
@@ -5,15 +5,16 @@ from pathlib import Path
 
 def create_async_boot(model_class: object, logger: logging, model_dir: str | Path):
     kwargs = {}
-    for arg in model_class.__init__.__annotations__:  # type: ignore
-        if "logger" in arg:
-            kwargs[arg] = logger
-        if "model_path" in arg:
-            kwargs[arg] = model_dir
-        if "model_dir" in arg:
-            kwargs[arg] = model_dir
-        if arg == "model":
-            kwargs[arg] = model_dir
+    if hasattr(model_class.__init__, "__annotations__"):
+        for arg in model_class.__init__.__annotations__:  # type: ignore
+            if "logger" in arg:
+                kwargs[arg] = logger
+            if "model_path" in arg:
+                kwargs[arg] = model_dir
+            if "model_dir" in arg:
+                kwargs[arg] = model_dir
+            if arg == "model":
+                kwargs[arg] = model_dir
 
     class AsyncBoot(model_class):
         def __init__(self, **kwargs):
@@ -27,6 +28,7 @@ def create_async_boot(model_class: object, logger: logging, model_dir: str | Pat
             await asyncio.to_thread(self._boot)
             if hasattr(self, "warmup"):
                 await asyncio.to_thread(self.warmup)
+
 
         def _boot(self):
             self.logger.info(f"Booting {self._class_name}")
--- src/fastmodel/utils/importer.py
+++ src/fastmodel/utils/importer.py
@@ -3,7 +3,7 @@
 import importlib
 import warnings
 from typing import Any, Tuple, Type
-
+import inspect
 
 class ImportFromStringError(Exception):
     pass
@@ -66,19 +66,43 @@ def validate_model_class(
     if not hasattr(model_class, "__call__"):
         raise ImportFromStringError(f"Model {model_class.__name__} must be callable.")
 
+    expected_model = None
     call_annotations = model_class.__call__.__annotations__
-    expected_model = call_annotations.get(annotation_key)
+    for annotation in [annotation_key, annotation_key + "_data"]:
+        expected_model = call_annotations.get(annotation)
+        if expected_model:
+            annotation_key = annotation
+            break
     if not expected_model:
-        annotation_key = annotation_key + "_data"
-        expected_model = call_annotations.get(annotation_key)
+        # This means that there is no annotation for the input or return value
+        # If the IO is given as a string, we can assign it to the input or return value
+        if annotation_key == "input":
+            for annotation in [annotation_key, annotation_key + "_data"]:
+                if inspect.signature(model_class.__call__).parameters.get(annotation):
+                    # This means we found the argument name for the input or return value
+                    expected_model = True if model_str else False
+                    annotation_key = annotation
+                    break
+        if annotation_key == "return":
+            if inspect.signature(model_class.__call__).return_annotation:
+                expected_model = True if model_str else False
+                annotation_key = "return"
+    
+    print(model_str, expected_model, annotation_key)
+    if not expected_model:
+        raise ImportFromStringError(f"No {annotation_key} model found in {model_class.__name__}. "\
+                                    f"Please provide the IO such as `fastmodel serve {model_class.__name__}:INPUT->OUTPUT`.")
+            
     selected_model = expected_model
     if model_str:
-        if model_str != expected_model.__name__:
-            selected_model = import_class(model_str)
+        if hasattr(expected_model, "__name__") and model_str != expected_model.__name__:
             if not issubclass(selected_model, expected_model):
                 warnings.warn(
                     f'Expected {annotation_key} model "{expected_model.__name__}" but got "{model_str}", proceeding but this may break the web server.'
                 )
+        else:
+            warnings.warn(f"{model_str} will be used as the {annotation_key} model.")
+        selected_model = import_class(model_str)
     elif expected_model:
         warnings.warn(
             f"No {annotation_key} model provided, using {selected_model.__name__} found in {model_class.__name__}."
